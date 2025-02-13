1d5838f25253d46b894fcf83eb2ee5605e23a231
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2025-02-13 09:45:52 +0700

    hello world

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,24 @@
+export default function HelloWorld () {
+    return (
+        <>
+            <HeaderHelloWorld />
+            <ParagraphHelloWorld />
+        </>
+    )
+}
+
+export function HeaderHelloWorld () {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+export function ParagraphHelloWorld() {
+    return (
+        <>
+            <h1>Selamat datang di dunia!</h1>
+        </>
+    )
+}
