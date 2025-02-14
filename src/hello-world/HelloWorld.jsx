8ccdeb3464b4e302af68835e1af9e36fb24f767e
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2025-02-14 08:42:32 +0700

    learn js to jsx and props

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,24 +1,31 @@
-export default function HelloWorld () {
+export default function HelloWorld() {
     return (
         <>
-            <HeaderHelloWorld />
+            <HeaderHelloWorld head = "Hello World!" />
             <ParagraphHelloWorld />
         </>
     )
 }
 
-export function HeaderHelloWorld () {
+export function HeaderHelloWorld({head = "Lupa kasih teks!!"}) {
     return (
         <>
-            <h1>Hello World</h1>
+            <h1 style={{
+                fontWeight: "bold",
+                letterSpacing: "5px"
+            }}>{head.toUpperCase()}</h1>
         </>
     )
 }
 
 export function ParagraphHelloWorld() {
+    const paragraph = "Selamat datang di dunia!";
+    const style = {
+        color: "blue",
+    }
     return (
         <>
-            <h1>Selamat datang di dunia!</h1>
+            <h1 style={style}>{paragraph}</h1>
         </>
     )
 }
